dd6c9d4b05e1823c682f868395165bae8a90d9cb
Akanksha <akankshapatil6687@gmail.com>
2023-07-16 16:37:56 +0530

    get data from route URL

--- Frontend/AngularUi/src/app/property/property-detail/property-detail.component.ts
+++ Frontend/AngularUi/src/app/property/property-detail/property-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-property-detail',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyDetailComponent implements OnInit {
 
-  constructor() { }
+  public propertyId!: number; 
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.propertyId = this.route.snapshot.params["id"];
   }
 
 }
