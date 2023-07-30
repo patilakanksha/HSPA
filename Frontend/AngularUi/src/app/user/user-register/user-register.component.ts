80cc28f6b4f006e44620229ae52afd71e25df078
Akanksha <akankshapatil6687@gmail.com>
2023-07-30 17:03:29 +0530

    Clean Code using FormBuilder

--- Frontend/AngularUi/src/app/user/user-register/user-register.component.ts
+++ Frontend/AngularUi/src/app/user/user-register/user-register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators, AbstractControl, ValidatorFn} from '@angular/forms';
+import { FormControl, FormGroup, Validators, AbstractControl, ValidatorFn, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -10,29 +10,77 @@ export class UserRegisterComponent implements OnInit {
 
   registrationForm!: FormGroup; 
   
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    /* //this code clean using formBuilder in createRegistrationForm()
     this.registrationForm = new FormGroup({
       userName: new FormControl(null, Validators.required),
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
       confirmPassword: new FormControl(null, Validators.required),
       mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)]),
-    }, this.passwordMatchingValidator() );
+    }, this.passwordMatchingValidator()
+    );
+    */
   }
 
+  createRegistrationForm()
+  {
+    this.registrationForm = this.fb.group(
+      {
+        userName : [null, Validators.required],
+        email : [null, [Validators.required, Validators.email]],
+        password : [null, [Validators.required, Validators.minLength(8)]],
+        confirmPassword:[null, Validators.required],
+        mobile: [null, [Validators.required, Validators.maxLength(10)]],
+      },
+      {
+        Validators : this.passwordMatchingValidator
+      }
+    )
+  } 
+  
+
    passwordMatchingValidator(): ValidatorFn {
+    
     return (control: AbstractControl): { [key: string]: any } | null => {
       const password = control.get('password')?.value;
       const confirmPassword = control.get('confirmPassword')?.value;
+      debugger
       const notmatched = password !== confirmPassword;
 
       // Return null if validation is successful
       // Return an object with an error code if validation fails
       return notmatched ? { notmatched: true } : null;
   }
-}
+ }
+
+//  passwordMatchingValidatior(fg: FormGroup): Validators {
+//   return fg.get('password')?.value === fg.get('confirmPassword')?.value ? null :
+//       {notmatched: true};
+// }
+
+//  Getter Methods for all controls
+ get userName(){
+  return this.registrationForm.get("userName") as FormControl;
+ }
+
+ get email(){
+  return this.registrationForm.get("email") as FormControl;
+ }
+
+ get password(){
+  return this.registrationForm.get("password") as FormControl;
+ }
+
+ get confirmPassword(){
+  return this.registrationForm.get("confirmPassword") as FormControl;
+ }
+
+ get mobile(){
+  return this.registrationForm.get("mobile") as FormControl;
+ }
 
   onSubmit(){
     console.log(this.registrationForm)
