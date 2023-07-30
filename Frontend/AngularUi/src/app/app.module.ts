09f975235bc232bfeb72556184a9c79d1699b823
Akanksha <akankshapatil6687@gmail.com>
2023-07-30 13:20:09 +0530

    Reactive Registration form created

--- Frontend/AngularUi/src/app/app.module.ts
+++ Frontend/AngularUi/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -12,12 +12,16 @@ import { PropertyListComponent } from './property/property-list/property-list.co
 import { HausingService } from './services/hausing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 
 const appRoutes: Routes=[
   {path:'', component:PropertyListComponent},
  
   {path:'add-property', component:AddPropertyComponent},
   {path:'property-detail/:id', component:PropertyDetailComponent},
+  {path:'user/register', component:UserRegisterComponent},
+  {path:'user/login', component:UserLoginComponent},
   {path:'**', component:PropertyListComponent},
 ]
 
@@ -28,13 +32,16 @@ const appRoutes: Routes=[
       NavBarComponent,
       PropertyListComponent,
       AddPropertyComponent,
-      PropertyDetailComponent
+      PropertyDetailComponent,
+      UserRegisterComponent,
+      UserLoginComponent
    ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [HausingService],
   bootstrap: [AppComponent]
