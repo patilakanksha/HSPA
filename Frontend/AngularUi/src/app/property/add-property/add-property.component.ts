d7aec81e32937901cc90077bbda00f1d6e91c8e7
Akanksha <akankshapatil6687@gmail.com>
2023-07-30 11:48:34 +0530

    validation for name field

--- Frontend/AngularUi/src/app/property/add-property/add-property.component.ts
+++ Frontend/AngularUi/src/app/property/add-property/add-property.component.ts
@@ -9,7 +9,8 @@ import {NgForm} from '@angular/forms';
 })
 export class AddPropertyComponent implements OnInit {
 
-  @ViewChild('Form') addPropertyForm: NgForm | undefined;
+  @ViewChild('Form')
+  addPropertyForm!: NgForm;
 
   constructor(private router: Router) { }
 
